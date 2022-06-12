f4c20b358e2f82f2b153fa35e1c4c39dffaf2cf7
pei.fan <fanpei920@gmail.com>
2022-06-12 12:30:31 +0900

    Update dev sample

--- dev/App.vue
+++ dev/App.vue
@@ -4,11 +4,36 @@
     <input v-model="end" type="text" />
   </p>
   <hr />
-  <range-calendar v-model:start="start" v-model:end="end" />
+  <range-calendar v-model:start="start" v-model:end="end" :options="options" />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Options } from '../src/types'
 const start = ref<Date | null>(null)
 const end = ref<Date | null>(null)
+const today = new Date()
+
+const options: Options = {
+  presets: [
+    {
+      text: 'Last 7 days',
+      modifier: () => {
+        return {
+          start: today,
+          end: new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000),
+        }
+      },
+    },
+    {
+      text: 'Last 30 days',
+      modifier: () => {
+        return {
+          start: today,
+          end: new Date(today.getTime() - 30 * 24 * 60 * 60 * 1000),
+        }
+      },
+    },
+  ],
+}
 </script>
